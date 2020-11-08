a2192b91af23feaee7429c473ea532fbaa747788
Karol Jurek <kjurek123@gmail.com>
2020-11-08 23:07:23 +0100

    Fix issue with relative imports

--- friends_service/src/db/database.py
+++ friends_service/src/db/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from ..settings import DB_URL
+from config.settings import DB_URL
 
 
 engine = create_engine(DB_URL)
