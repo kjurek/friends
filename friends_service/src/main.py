068f7b9982ece22a927a82b7950cb847e651b691
Karol Jurek <kjurek123@gmail.com>
2020-11-09 11:21:24 +0100

    Make application paths consistent between test and dev

--- friends_service/src/main.py
+++ friends_service/src/main.py
@@ -1,25 +1,28 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Path
 from sqlalchemy.orm import Session
-import uuid
 
-from db.database import engine, get_db
-from db import models, crud
+from .db.database import engine, get_db
+from .db import models, crud
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+USER_ID_PATH = Path(..., title="Non-negative integer which identifies the user", ge=0)
+FRIEND_ID_PATH = Path(..., title="Non-negative integer which identifies users friend", ge=0)
 
 @app.get("/friends/{user_id}")
-async def get_friends(user_id: uuid.UUID, db: Session = Depends(get_db)):
+async def get_friends(user_id: int = USER_ID_PATH, db: Session = Depends(get_db)):
     return crud.get_friends(db, user_id)
 
 
 @app.post("/friends/{user_id}/{friend_id}")
-async def add_friend(user_id: uuid.UUID, friend_id: uuid.UUID, db: Session = Depends(get_db)):
+async def add_friend(user_id: int = USER_ID_PATH, friend_id: int = FRIEND_ID_PATH,
+                     db: Session = Depends(get_db)):
     crud.add_friend(db, user_id, friend_id)
 
 
 @app.delete("/friends/{user_id}/{friend_id}")
-async def remove_friend(user_id: uuid.UUID, friend_id: uuid.UUID, db: Session = Depends(get_db)):
+async def remove_friend(user_id: int = USER_ID_PATH, friend_id: int = FRIEND_ID_PATH,
+                        db: Session = Depends(get_db)):
     crud.remove_friend(db, user_id, friend_id)
