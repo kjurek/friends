b68bec7a784fdc8bd3448c43474b7247a60e1282
kjurek <kjurek123@gmail.com>
2020-11-09 17:17:57 +0100

    Remove redundant entries from db (#17)
    * Remove redundant entries from db
    
    * fix typo
    
    * add missing whitespace
    
    * add integration test for invalid id

--- friends_service/tests/db/test_crud.py
+++ friends_service/tests/db/test_crud.py
@@ -19,7 +19,6 @@ def test_add_friend_one_friend(database):
     assert crud.add_friend(database, 0, 1) is True
     assert get_all_friendships(database) == {
         0: [1],
-        1: [0]
     }
 
 
@@ -29,27 +28,20 @@ def test_add_friend_friendship_with_self_takes_no_effect(database):
 
 
 def test_add_friend_already_existing_friendship_takes_no_effect(database):
-    friendship_models = [
-        models.Friendship(user_id=0, friend_id=1),
-        models.Friendship(user_id=1, friend_id=0),
-    ]
-    database.add_all(friendship_models)
+    database.add(models.Friendship(user_id=0, friend_id=1))
     database.commit()
     assert get_all_friendships(database) == {
         0: [1],
-        1: [0],
     }
 
     assert crud.add_friend(database, 0, 1) is False
     assert get_all_friendships(database) == {
         0: [1],
-        1: [0],
     }
 
     assert crud.add_friend(database, 1, 0) is False
     assert get_all_friendships(database) == {
         0: [1],
-        1: [0],
     }
 
 
@@ -57,24 +49,17 @@ def test_add_friend_multiple_friends(database):
     assert crud.add_friend(database, 0, 1) is True
     assert crud.add_friend(database, 0, 2) is True
     assert crud.add_friend(database, 1, 2) is True
-
     assert get_all_friendships(database) == {
         0: [1, 2],
-        1: [0, 2],
-        2: [0, 1],
+        1: [2],
     }
 
 
 def test_remove_friend_one_friend(database):
-    friendship_models = [
-        models.Friendship(user_id=0, friend_id=1),
-        models.Friendship(user_id=1, friend_id=0),
-    ]
-    database.add_all(friendship_models)
+    database.add(models.Friendship(user_id=0, friend_id=1))
     database.commit()
     assert get_all_friendships(database) == {
         0: [1],
-        1: [0],
     }
 
     assert crud.remove_friend(database, 0, 1) is True
@@ -82,27 +67,20 @@ def test_remove_friend_one_friend(database):
 
 
 def test_remove_friend_non_existing_friendship(database):
-    friendship_models = [
-        models.Friendship(user_id=0, friend_id=1),
-        models.Friendship(user_id=1, friend_id=0),
-    ]
-    database.add_all(friendship_models)
+    database.add(models.Friendship(user_id=0, friend_id=1))
     database.commit()
     assert get_all_friendships(database) == {
         0: [1],
-        1: [0],
     }
 
     assert crud.remove_friend(database, 0, 2) is False
     assert get_all_friendships(database) == {
         0: [1],
-        1: [0],
     }
 
     assert crud.remove_friend(database, 2, 0) is False
     assert get_all_friendships(database) == {
         0: [1],
-        1: [0],
     }
 
 
@@ -110,30 +88,24 @@ def test_remove_friend_multiple_friends(database):
     friendship_models = [
         models.Friendship(user_id=0, friend_id=1),
         models.Friendship(user_id=0, friend_id=2),
-        models.Friendship(user_id=1, friend_id=0),
         models.Friendship(user_id=1, friend_id=2),
-        models.Friendship(user_id=2, friend_id=0),
-        models.Friendship(user_id=2, friend_id=1),
     ]
     database.add_all(friendship_models)
     database.commit()
     assert get_all_friendships(database) == {
         0: [1, 2],
-        1: [0, 2],
-        2: [0, 1],
+        1: [2],
     }
 
     assert crud.remove_friend(database, 0, 1) is True
     assert get_all_friendships(database) == {
         0: [2],
         1: [2],
-        2: [0, 1],
     }
 
     assert crud.remove_friend(database, 0, 2) is True
     assert get_all_friendships(database) == {
         1: [2],
-        2: [1],
     }
 
     assert crud.remove_friend(database, 2, 1) is True
@@ -141,15 +113,10 @@ def test_remove_friend_multiple_friends(database):
 
 
 def test_get_friends_one_friend(database):
-    friendship_models = [
-        models.Friendship(user_id=0, friend_id=1),
-        models.Friendship(user_id=1, friend_id=0),
-    ]
-    database.add_all(friendship_models)
+    database.add(models.Friendship(user_id=0, friend_id=1))
     database.commit()
     assert get_all_friendships(database) == {
         0: [1],
-        1: [0],
     }
     assert crud.get_friends(database, 0) == [1]
     assert crud.get_friends(database, 1) == [0]
@@ -165,43 +132,32 @@ def test_get_friends_multiple_friends(database):
     friendship_models = [
         models.Friendship(user_id=0, friend_id=1),
         models.Friendship(user_id=0, friend_id=2),
-        models.Friendship(user_id=1, friend_id=0),
         models.Friendship(user_id=1, friend_id=2),
-        models.Friendship(user_id=2, friend_id=0),
-        models.Friendship(user_id=2, friend_id=1),
     ]
     database.add_all(friendship_models)
     database.commit()
     assert get_all_friendships(database) == {
         0: [1, 2],
-        1: [0, 2],
-        2: [0, 1],
+        1: [2],
     }
-    assert crud.get_friends(database, 0) == [1, 2]
-    assert crud.get_friends(database, 1) == [0, 2]
-    assert crud.get_friends(database, 2) == [0, 1]
+    assert sorted(crud.get_friends(database, 0)) == [1, 2]
+    assert sorted(crud.get_friends(database, 1)) == [0, 2]
+    assert sorted(crud.get_friends(database, 2)) == [0, 1]
 
 
 def test_remove_friend_friendship_with_self_takes_no_effect(database):
-    friendship_models = [
-        models.Friendship(user_id=0, friend_id=1),
-        models.Friendship(user_id=1, friend_id=0),
-    ]
-    database.add_all(friendship_models)
+    database.add(models.Friendship(user_id=0, friend_id=1))
     database.commit()
     assert get_all_friendships(database) == {
         0: [1],
-        1: [0],
     }
 
     assert crud.remove_friend(database, 0, 0) is False
     assert get_all_friendships(database) == {
         0: [1],
-        1: [0],
     }
 
     assert crud.remove_friend(database, 1, 1) is False
     assert get_all_friendships(database) == {
         0: [1],
-        1: [0],
     }
