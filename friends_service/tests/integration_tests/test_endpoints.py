86c0c231250b3db4f08af42d529459bdb46b9e67
Karol Jurek <kjurek123@gmail.com>
2020-11-10 16:52:29 +0100

    Fix error responses

--- friends_service/tests/integration_tests/test_endpoints.py
+++ friends_service/tests/integration_tests/test_endpoints.py
@@ -132,10 +132,18 @@ def test_remove_reversed_friendship(test_client):
 def test_add_friend_with_userid_and_friend_id_equal_returns_code_422(test_client):
     response = test_client.post("/friends/0/0")
     assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
-    assert response.json() == {"reason": "user_id [0] cannot be the same as friend_id [0]"}
+    assert response.json() == {
+        "detail": [{"loc": ["path", "user_id"],
+                    "msg": f"user_id [0] cannot be the same as friend_id [0]",
+                    "type": "value_error"}]
+    }
 
 
 def test_remove_friend_with_userid_and_friend_id_equal_returns_code_422(test_client):
     response = test_client.delete("/friends/0/0")
     assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
-    assert response.json() == {"reason": "user_id [0] cannot be the same as friend_id [0]"}
+    assert response.json() == {
+        "detail": [{"loc": ["path", "user_id"],
+                    "msg": f"user_id [0] cannot be the same as friend_id [0]",
+                    "type": "value_error"}]
+    }
